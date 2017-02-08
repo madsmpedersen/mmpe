9d5cd94540a73587db8b67555a8836cd1cea99a8
Mads M. Pedersen <mmpe@dtu.dk>
2017-02-08 11:13:00 +0100

    updated path in test_inspect_ext.py

--- functions/unit_tests/test_inspect_ext.py
+++ functions/unit_tests/test_inspect_ext.py
@@ -9,12 +9,12 @@ from mmpe.functions import class_list, argument_string
 import numpy as np
 
 
-os.chdir("../../")
 
+tfp = os.path.join(os.path.dirname(__file__))
 class Test(unittest.TestCase):
 
     def test_class_list(self):
-        lst = [cls.__name__ for cls in class_list("algorithms", object)]
+        lst = [cls.__name__ for cls in class_list(os.path.join(os.path.dirname(__file__)+"/../../algorithms"), object)]
         for cls in ['Score', 'IgnoreCaseMatch', 'StringMatch', 'SmartMatch', 'SimpleMatch', 'Score', 'SimpleMatch', 'StringMatch', 'Score', 'SmartMatch', 'IgnoreCaseMatch', 'SmartMatch']:
             self.assertTrue(cls in lst, "%s is not found"%cls)
 
