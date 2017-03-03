62e95620fb0166cb239c86aa5bdf32f6c688dca2
Mads M. Pedersen <mmpe@dtu.dk>
2017-03-03 08:34:11 +0100

    pyinstaller make exe update

--- build_exe/pyinstaller/build_exe.py
+++ build_exe/pyinstaller/build_exe.py
@@ -2,9 +2,15 @@ import os
 import sys
 
 
-def build_exe(filename, version="1.0.0", description="", author="", modules=[], includes=[], packages="[]", include_files=[], zip_includes=[], icon=None, additional_executables=[], target=None):
+def build_exe(filename, version="1.0.0", description="", author="", modules=[], includes=[], packages="[]", include_files=[], zip_includes=[], icon=None, additional_executables=[], gui=True,onefile=False, name=None):
     basename = filename.replace('.py', '')
-    cmd = '%s/scripts/pyinstaller.exe  -F --noconsole %s' % (os.path.dirname(sys.executable), filename)
+    args = []
+    if onefile: args.append("--onefile")
+    if gui: args.append("--noconsole")
+    if name: args.append("-n "+name)
+    if icon: args.append('-i '+ icon)
+        
+    cmd = '%s/scripts/pyinstaller.exe %s %s' % (os.path.dirname(sys.executable)," ".join(args), filename)
     print (cmd)
     os.system(cmd)
     print ("finish")
